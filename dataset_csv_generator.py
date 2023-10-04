73d0e4b752a5d15782e3cfed52db2616e55874db
Maroof Abdul <abdul@lfb.rwth-aachen.de>
2023-10-04 17:08:25 +0200

    Initial commit

--- dataset_csv_generator.py
+++ dataset_csv_generator.py
@@ -3,8 +3,8 @@ import os
 import numpy as np
 import csv
 
-input_csv_path = "/work/scratch/abdul/CLAM/patches_RESULTS/process_list_autogen_EDITED_FEATURES.csv"
-output_csv_path = '/work/scratch/abdul/CLAM/tumor_vs_normal_dummy_clean.csv'
+input_csv_path = '/work/scratch/abdul/CLAM/Renal/patches_RESULTS/COMBINED_process_list_autogen_SELECTED.csv'
+output_csv_path = '/work/scratch/abdul/CLAM/Renal/tumor_vs_normal_combined_clean.csv'
 df1 = pd.read_csv(input_csv_path)
 slide_ids = df1['slide_id'].to_numpy()
 
